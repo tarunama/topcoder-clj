2f8758604f43c495b071d1d1d60f04b0a3d9b2a0
meganeski <yamamoto@marici.co.jp>
2016-01-09 15:55:41 +0900

    fix test file.

--- test/topcoder/div2/fox_and_game_test.clj
+++ test/topcoder/div2/fox_and_game_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [topcoder.div2.fox_and_game :refer :all]))
 
-(deftest test-fox-and-game []
-  (is (= (count-stars '("ooo" "ooo")) 6)
-      (= (count-stars '("xxo" "xxx")) 1)))
+(deftest test-fox-and-game
+  (is (= (count-stars '("ooo" "ooo")) 6))
+  (is (= (count-stars '("xxo" "xxx")) 1)))
