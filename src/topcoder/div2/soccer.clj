973caa1fb961624319c9a3d86e5d302f7054dd11
meganeski <yamamoto@marici.co.jp>
2016-01-11 11:50:41 +0900

    Add abnormal testcase.

--- src/topcoder/div2/soccer.clj
+++ src/topcoder/div2/soccer.clj
@@ -1,13 +1,14 @@
-(ns topcoder.div2.soccer)
+(ns topcoder.div2.soccer
+  (:require [clojure.test :refer [is]]))
 
 ; problem description
 ; https://community.topcoder.com/stat?c=problem_statement&pm=1650
-; TODO: 例外処理の追加
 
 (defn calc-points [win tie]
   (apply + [(* win 3) tie]))
 
 (defn max-points [wins ties]
+  {:pre [(and (coll? wins) (coll? ties))]}
   (loop [wins wins
          ties ties
          ret ()]
