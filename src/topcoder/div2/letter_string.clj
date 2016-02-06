6100c7c2e1ca093243a6471a1d8f9eabdbad0b2d
meganeski <yamamoto@marici.co.jp>
2016-02-06 22:06:04 +0900

    Separate function.

--- src/topcoder/div2/letter_string.clj
+++ src/topcoder/div2/letter_string.clj
@@ -1,4 +1,7 @@
 (ns topcoder.div2.letter-string)
 
+(defn get-only-string [s]
+  (mapcat #(re-seq #"[a-zA-Z]" %) s))
+
 (defn sum [& s]
-  (count (apply str (mapcat #(re-seq #"[a-zA-Z]" %) s))))
+  (count (apply str (get-only-string s))))
--- test/topcoder/div2/letter_string_test.clj
+++ test/topcoder/div2/letter_string_test.clj
@@ -2,6 +2,13 @@
   (:require [topcoder.div2.letter-string :refer :all]
             [clojure.test :refer :all]))
 
+(deftest test-get-only-string
+  (is (= (get-only-string '("-")) ()))
+  (is (= (get-only-string '("A")) '("A")))
+  (is (= (get-only-string '("-----Abc")) '("A" "b" "c")))
+  (is (= (get-only-string '("-A-B-C-D")) '("A" "B" "C" "D")))
+  )
+
 (deftest test-sum
   (is (= (sum "-") 0))
   (is (= (sum "A") 1))
